11bd101bf1edf316ddd76044f17d9e66636edfc5
nickway90 <king.nickway90@gmail.com>
2019-01-12 09:30:33 -0700

    installed swagger. user routes somewhat working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
     skip_before_action :authorize_request, only: :create
+    swagger_controller :users, 'User Management'
 
     # POST /signup
+    swagger_api :create do 
+        summary 'New user signup'
+        notes 'Create a new user account in order to start managing your record collection.'
+        param :form, 'user[name]', :string, :required, 'Full name'
+        param :form, 'user[email]', :string, :required, 'Email address'
+        param :form, 'user[password]', :string, :required, 'Password'
+        param :form, 'user[password_confirmation]', :string, :required, 'Password confirmation'
+        response :created
+        response :unprocessable_entity
+    end
     def create
         user = User.create!(user_params)
         auth_token = AuthenticateUser.new(user.email, user.password).call
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 rake db:create
 rake db:migrate
+SD_LOG_LEVEL=1 rake swagger:docs
 
 puma -C config/puma.rb
