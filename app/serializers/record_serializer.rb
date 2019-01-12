76b73d07036f31e6473f6acdf4db167e967407d9
nickway90 <king.nickway90@gmail.com>
2019-01-11 22:44:12 -0700

    adding timestamps to serializer

--- app/serializers/record_serializer.rb
+++ app/serializers/record_serializer.rb
@@ -1,4 +1,4 @@
 class RecordSerializer < ActiveModel::Serializer
-  attributes :id, :artist, :album, :year_released, :year_printed, :condition
+  attributes :id, :artist, :album, :year_released, :year_printed, :condition, :created_at, :updated_at
   has_many :tracks
 end
