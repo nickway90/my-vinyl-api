8067adfd4acc7b6fced0bbcdb81764f630314d34
nickway90 <king.nickway90@gmail.com>
2019-01-12 18:44:34 -0700

    removed api-docs from route for swag

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -4,7 +4,7 @@ RSpec.configure do |config|
   # Specify a root folder where Swagger JSON files are generated
   # NOTE: If you're using the rswag-api to serve API descriptions, you'll need
   # to ensure that it's configured to serve Swagger from the same folder
-  config.swagger_root = Rails.root.to_s + '/public/api-docs'
+  config.swagger_root = Rails.root.to_s + '/public'
 
   # Define one or more Swagger documents and provide global metadata for each one
   # When you run the 'rswag:specs:to_swagger' rake task, the complete Swagger will
