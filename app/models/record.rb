7196a46282211859907bc35d488a841ec154f45e
nickway90 <king.nickway90@gmail.com>
2019-01-14 20:08:15 -0700

    adding more model validation to records

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,7 +1,21 @@
 class Record < ApplicationRecord
     # A Record has many Tracks
     has_many :tracks, dependent: :destroy
+
+    # Years must be between or equal to 1900-2019
+    validates :year_printed, :year_released, numericality: {
+        only_integer: true, 
+        less_than_or_equal_to: 2019, 
+        greater_than_or_equal_to: 1900
+    }
+
+    # Condition must be between or equal to 0-10
+    validates :condition, numericality: {
+        only_integer: true,
+        less_than_or_equal_to: 10,
+        greater_than_or_equal_to: 0
+    }
     
     # Required fields
-    validates_presence_of :artist, :album, :year_released, :year_printed, :condition
+    validates :artist, :album, :year_released, :year_printed, :condition, presence: true
 end
